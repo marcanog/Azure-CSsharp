a62844977069f5c57f170550d88c9cdc4fbb5cec
Georgelys Marcano <57365496+marcanog@users.noreply.github.com>
2025-06-18 21:05:51 +0000

    update  AssemblyInfo

--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26c58e60900e282410e265934f72f6baff48abd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbe289a8faf946006147781937a9f8255c53c04f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
