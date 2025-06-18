cbe289a8faf946006147781937a9f8255c53c04f
Georgelys Marcano <57365496+marcanog@users.noreply.github.com>
2025-06-18 20:41:41 +0000

    make case code 2 of challenge

--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859fe91dae5ef4154331031165148d2e9fe4e513")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26c58e60900e282410e265934f72f6baff48abd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
