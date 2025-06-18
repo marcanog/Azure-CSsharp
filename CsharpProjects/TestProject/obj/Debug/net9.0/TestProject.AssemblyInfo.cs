2bd2dbf2f9b6b280c7b4f143c5d3af886fd6af11
Georgelys Marcano <57365496+marcanog@users.noreply.github.com>
2025-06-18 21:42:30 +0000

    change file because sdk 9

--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e8cbddb4a1691b9111a2d6fd9f000d94ad2c86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aac58fdaa5f573298beb71aeb790d7821cb3869")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
